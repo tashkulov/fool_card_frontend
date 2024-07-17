ab5ab20ac72d94f3fd485da3c3ce926a518311f7
Germanwin <grishingerman7766005@gmail.com>
2024-07-17 22:47:02 +0300

    made the right routing

--- client/src/app/home-page/home-page.component.ts
+++ client/src/app/home-page/home-page.component.ts
@@ -1,16 +1,25 @@
 import { Component, Input } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterOutlet,
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
   @Input() user: any;
 
+  constructor(private router: Router) {}
+
   ngOnInit() {
     console.log('User in NewGameComponent:', this.user); // Log user data in NewGameComponent
+  }
+
+  navigateTo(path: string) {
+    this.router.navigate([path]);
   }
 }
