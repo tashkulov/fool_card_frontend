4d987e807746361d06d95c5550ef5388e923c377
Germanwin <grishingerman7766005@gmail.com>
2024-07-26 10:57:57 +0300

    w

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,14 @@ import axios from "axios";
 // const [cookies] = useCookies(['authorization']);
 export const $api = axios.create({
     baseURL: "https://foolcard2.shop",
+
+    
     // headers: {
     //     Authorization: cookies.authorization
     // }
 });
+
+$api.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`
+    return config
+})
