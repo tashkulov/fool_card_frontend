88abfb11b35dc7c13f2cdf6b1e41cd759716af2e
Arsen0312 <greenchay16@gmail.com>
2024-07-29 19:59:18 +0600

    fix scroll in Kvesty page

--- src/app/pages/kwesty/Kvesty.tsx
+++ src/app/pages/kwesty/Kvesty.tsx
@@ -1,18 +1,18 @@
-import './Kvesty.css';
 import kvest1 from '../img/kvest1.svg';
 import kvest2 from '../img/kvest2.svg';
 import kvest3 from '../img/kvesty3.svg';
 import OneKvest from "../../components/OneKvest/OneKvest";
 import SkeletonPage from "../../components/SkeletonPage/ui/SkeletPage";
 import {useTranslation} from "react-i18next";
+import cls from "./Kvesty.module.scss"
 
 const Kvesty = () => {
     const {t} = useTranslation()
 
     return (
     <SkeletonPage textHeader={t("Квесты")}>
-        <div className='container-kwest'>
-            <div className='kvesty-list'>
+        <div className={cls.containerKwest}>
+            <div className={cls.kvestyList}>
                 <OneKvest image={kvest1} headText="Выиграть 5000 монет" description="XP 30.5" buttonText='Начать'/>
                 <OneKvest image={kvest2} headText="Выиграть 2342 монет" description="XP 30.5" buttonText='Начать'/>
                 <OneKvest image={kvest3} headText="Выиграть 5000 монет" description="XP 30.5"
@@ -21,6 +21,18 @@ const Kvesty = () => {
                           buttonText='Получить'/>
                 <OneKvest image={kvest3} headText="Выиграть 5000 монет" description="XP 30.5"
                           buttonText='Получить'/>
+                <OneKvest image={kvest3} headText="Выиграть 523 монет" description="XP 30.5" buttonText='Начать'/>
+                <OneKvest image={kvest3} headText="Выиграть 523 монет" description="XP 30.5" buttonText='Начать'/>
+                <OneKvest image={kvest3} headText="Выиграть 523 монет" description="XP 30.5" buttonText='Начать'/>
+                <OneKvest image={kvest3} headText="Выиграть 523 монет" description="XP 30.5" buttonText='Начать'/>
+                <OneKvest image={kvest3} headText="Выиграть 523 монет" description="XP 30.5" buttonText='Начать'/>
+                <OneKvest image={kvest3} headText="Выиграть 523 монет" description="XP 30.5" buttonText='Начать'/>
+                <OneKvest image={kvest3} headText="Выиграть 523 монет" description="XP 30.5" buttonText='Начать'/>
+                <OneKvest image={kvest3} headText="Выиграть 523 монет" description="XP 30.5" buttonText='Начать'/>
+                <OneKvest image={kvest3} headText="Выиграть 523 монет" description="XP 30.5" buttonText='Начать'/>
+                <OneKvest image={kvest3} headText="Выиграть 523 монет" description="XP 30.5" buttonText='Начать'/>
+                <OneKvest image={kvest3} headText="Выиграть 523 монет" description="XP 30.5" buttonText='Начать'/>
+                <OneKvest image={kvest3} headText="Выиграть 523 монет" description="XP 30.5" buttonText='Начать'/>
                 <OneKvest image={kvest3} headText="Выиграть 523 монет" description="XP 30.5" buttonText='Начать'/>
             </div>
         </div>
