fa573cd024fba9174c1d3b51f21db18125acd317
tashkulov <nurislamtashkulov168@gmail.com>
2024-07-30 15:40:21 +0600

    everything is bad

--- src/app/pages/play-game/play-game.tsx
+++ src/app/pages/play-game/play-game.tsx
@@ -7,9 +7,11 @@ import coins from "../img/coins.svg";
 import arrow from "../img/Arrow1.svg";
 import { useEffect, useState, useRef } from "react";
 import back_card from '../../../assets/cards/back/back_3.svg';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { fetchGameData, fetchGameList, placeCardOnTable, beatCard, endTurn } from './apiService'; // Импортируем функции API
 import { GameData, GameListItem } from './interface';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const PlayGame = () => {
     const [gameData, setGameData] = useState<GameData | null>(null);
@@ -24,8 +26,7 @@ const PlayGame = () => {
     const [tableCards, setTableCards] = useState<{ card: string, beaten_by_card: string | null }[]>([]);
     const [attackMode, setAttackMode] = useState<boolean>(true);
 
-    const gameId = 39;
-
+    const gameId = 41;
 
     const loadGameData = async () => {
         try {
@@ -92,6 +93,11 @@ const PlayGame = () => {
     };
 
     const endTurnHandler = async () => {
+        if (hasUnbeatenCards()) {
+            toast.error('Необходимо побить все карты на столе, прежде чем завершить ход.');
+            return;
+        }
+
         try {
             await endTurn(gameId);
             setTableCards([]);
@@ -144,6 +150,10 @@ const PlayGame = () => {
                 }
             }
         }
+    };
+
+    const hasUnbeatenCards = () => {
+        return tableCards.some(card => card.beaten_by_card === null);
     };
 
     if (loading) return <div>Loading...</div>;
@@ -325,9 +335,17 @@ const PlayGame = () => {
                     </div>
                 </div>
                 <div className="play-footer-wrap">
-                        <button className="play-footer-btn" onClick={endTurnHandler}>Бито</button>
+                    <button
+                        className="play-footer-btn"
+                        onClick={endTurnHandler}
+                        disabled={hasUnbeatenCards()}
+                    >
+                        Бито
+                    </button>
                 </div>
             </div>
+
+            <ToastContainer />
         </div>
     );
 };
