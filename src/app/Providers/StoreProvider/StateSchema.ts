2c32c18ce5514b357755922c2f43ce5c6650ac51
tashkulov <nurislamtashkulov168@gmail.com>
2024-08-01 14:11:53 +0600

    to deploy 2

--- src/app/Providers/StoreProvider/StateSchema.ts
+++ src/app/Providers/StoreProvider/StateSchema.ts
@@ -1,9 +1,7 @@
 import { AxiosInstance } from "axios";
-import {IFooterState, stateFooterReducerName} from "../../Entites/stateFooter/slice/stateFooter";
 
 
 export interface StateSchema {
-    [stateFooterReducerName]: IFooterState
 }
 
 export interface ThunkExtraArg {
--- src/app/Providers/StoreProvider/store.ts
+++ src/app/Providers/StoreProvider/store.ts
@@ -1,8 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { stateFooterReducer } from "../../Entites/stateFooter/slice/stateFooter";
 
 const rootReducer = combineReducers({
-    stateFooter: stateFooterReducer
 });
 
 export const setupStore = () => {
