8992af6316eb2a5bc651c69f2532de1693d7b29d
Germanwin <grishingerman7766005@gmail.com>
2024-08-14 10:36:16 +0300

    some changes

---
+++ src/app/pages/home-page/components/home-page-header.tsx
@@ -0,0 +1,39 @@
+import { store } from "../../play-game/ui/store"
+
+
+const HomePageHeader: React.FC = () => {
+    return (
+        <div className="main-page-header">
+            <div className="main-page-header-content">
+                <div className="main-page-header-content-avatar-border">
+                    <div className="main-page-header-content-avatar">
+                        <img src={store.getState().user.photo_url} />
+                    </div>
+                </div>
+                <div className="main-page-header-content-data">
+                    <div className="main-page-header-content-data-username">
+                        {store.getState().user.first_name || "Guest"}
+                    </div>
+                    <div className="main-page-header-content-data-credits">
+                        <div className="main-page-header-content-data-credits-1">
+                            <div className="main-page-header-content-data-credits-1-icon"></div>
+                            <div className="main-page-header-content-data-credits-1-value">
+                                100K
+                            </div>
+                        </div>
+                        <div className="main-page-header-content-data-credits-1">
+                            <div className="main-page-header-content-data-credits-2-icon"></div>
+                            <div className="main-page-header-content-data-credits-1-value">
+                                152.5K
+                            </div>
+                            <div></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default HomePageHeader
