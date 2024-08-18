e1c30c8d149be20bff6eaa0bdd22d233013cab94
Germanwin <grishingerman7766005@gmail.com>
2024-08-18 22:07:02 +0300

    fixed build

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
 import { AppRouter } from "./Router";
 import cls from "./main.module.scss";
 import Footer from './components/Footer/Footer';
 import { useLocation } from "react-router-dom";
-import {init_sockets , disconnectFromSocket } from '../socket.ts'; // Импортируем функции подключения к WebSocket
-import {hasLoggedIn, LoginUser, RegisterUser} from "./authorization.ts";
+// import {init_sockets , disconnectFromSocket } from '../socket.ts'; // Импортируем функции подключения к WebSocket
+import {RegisterUser} from "./authorization.ts";
 
 const App: React.FC = () => {
     const location = useLocation();
