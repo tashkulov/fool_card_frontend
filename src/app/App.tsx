fd4d1de753691d4e99062489d19b07db7243cc57
Arsen0312 <greenchay16@gmail.com>
2024-08-18 16:01:09 +0600

    add reg user with telegram

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { AppRouter } from "./Router";
 import cls from "./main.module.scss";
 import "../../src/app/I18NEXT/i18n/i18n"
 import Footer from './components/Footer/Footer';
 import {useLocation} from "react-router-dom";
 
-export const user = {
-    photo_url: 'URL_TO_PHOTO',
-    first_name: 'Имя пользователя',
-};
-
 const App: React.FC = () => {
     const location = useLocation()
+
+    useEffect(() => {
+        const RegisterUser = async () => {
+            try {
+                if (window.Telegram && window.Telegram.WebApp) {
+                    const initData = window.Telegram.WebApp.initData;
+                    console.log(initData);
+
+                    if (initData) {
+                        const userData = {
+                            init_data: initData,
+                        };
+
+                        // Отправка данных на сервер
+                        const response = await axios.post('/receiver/authorize', userData);
+                        console.log('Ответ:', response.data);
+
+                        hasRegistered.current = true;
+
+                        localStorage.setItem("token", response.data.token);
+                        console.log(localStorage.getItem("authorization"));
+                    } else {
+                        console.error('Не удалось получить данные пользователя');
+                    }
+                } else {
+                    console.error('Telegram Web App SDK не загружен');
+                }
+            } catch (error) {
+                console.error('Ошибка при отправке запроса:', error);
+            }
+        };
+
+        RegisterUser()
+    }, []);
+
 
     if (location.pathname.split("/")[1] === "inGame") {
         return (
