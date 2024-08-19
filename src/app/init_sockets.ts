5631081a47a89385690aecd4cb61d2ddbec4d363
Germanwin <grishingerman7766005@gmail.com>
2024-08-19 13:56:37 +0300

    dfg

--- src/app/init_sockets.ts
+++ src/app/init_sockets.ts
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 export function init_socket(jtoken : string) {
     const socket = io('https://foolcard2.shop/',
         {
-            path: '/reciever/wsg',
+            path: '/coordinator/wsg',
             auth: {
                 token: jtoken
             }
